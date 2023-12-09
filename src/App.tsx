713abe700dbe66f73d3afdd5185e2fb6a57df666
Hi <huydao1008@gmail.com>
2023-12-09 14:16:41 +0700

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import './assets/styles/App.css'
-import SignInPage from './pages/auth/SignInPage'
+
 import AuthRequire from './features/authRequire'
 import WelcomePage from './features/Welcome'
 import DashboardPage from './pages/DashboardPage'
 import { PersonalPage } from './pages/personal/PersonalPage'
+import SignIn from './pages/auth/SignIn'
+import SignUp from './pages/auth/SignUp'
 
 function App(): JSX.Element {
   return (
@@ -12,7 +14,8 @@ function App(): JSX.Element {
       <Route path='/' element={<DashboardPage />} />
       {/* public route */}
       <Route index element={<DashboardPage />} />
-      <Route path='login' element={<SignInPage />} />
+      <Route path='login' element={<SignIn />} />
+      <Route path='signup' element={<SignUp />} />
 
       <Route path='personal_information' element={<PersonalPage />} />
 
---
+++ src/pages/auth/SignIn.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import classes from './SignIn.module.css'
+import TextInputField from './components/TextInputField'
+import { Link } from 'react-router-dom'
+import logo from './../../assets/images/Logo.png'
+import facebook from './../../assets/images/logos_facebook.svg'
+import google from './../../assets/images/devicon_google.svg'
+
+const SignIn = () => {
+  const [enteredEmail, setEnteredEmail] = React.useState('')
+  const [enteredPassword, setEnteredPassword] = React.useState('')
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    console.log(enteredEmail, enteredPassword)
+  }
+
+  const changeEmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredEmail(event.target.value)
+  }
+
+  const changePasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredPassword(event.target.value)
+  }
+
+  return (
+    <div className={classes.login}>
+      <img
+        className={classes['image-cover']}
+        src='https://hips.hearstapps.com/hmg-prod/images/1271-saint-ives-place-high-resolution-73-1666640275.jpg?crop=0.668xw:1.00xh;0.202xw,0&resize=1200:*'
+        alt='Cover Image'
+      />
+
+      <div className={classes.form}>
+        {/* <form onSubmit={submitHandler}> */}
+        <div className={classes.logo}>
+          <img src={logo} alt='logo' />
+        </div>
+        <h2>Đăng nhập</h2>
+        <form onSubmit={submitHandler}>
+          <div className={`${classes.control} `}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              placeholder='Email'
+              value={enteredEmail}
+              onChange={changeEmailHandler}
+            />
+          </div>
+          <div className={`${classes.control} `}>
+            <label htmlFor='password'>Mật khẩu</label>
+            <input
+              type='password'
+              id='password'
+              placeholder='Mật khẩu'
+              value={enteredPassword}
+              onChange={changePasswordHandler}
+            />
+          </div>
+          <div className={`${classes.control} `}>
+            <a href='/'>Quên mật khẩu?</a>
+          </div>
+          <div className={classes.actions}>
+            <button type='submit'>ĐĂNG NHẬP</button>
+          </div>
+
+          <div className={classes.control}>
+            <span>Hoặc đăng nhập bằng</span>
+          </div>
+
+          <div className={classes['social-button']}>
+            <button>
+              <img src={facebook} alt='Logo' />
+              Facebook
+            </button>
+            <button>
+              <img src={google} alt='Logo' /> Google
+            </button>
+          </div>
+
+          <div className={classes.control}>
+            <span>
+              Chưa có tài khoản? <Link to='/signup'>Đăng ký tài khoản mới</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
