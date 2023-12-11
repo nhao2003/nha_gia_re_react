54b2472b5744fedb22664f4d3d52ad6b3ecb0816
Hi <huydao1008@gmail.com>
2023-12-12 00:46:04 +0700

    auth page

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,18 +1,28 @@
-import { Route, Routes } from "react-router-dom";
-import { ModernDashboardPage } from "./modern/modules/ModernDashboardPage";
-import { ModernHomePage } from "./modern/modules/home/modern_home_page";
-import { ProfilePage } from "./modern/modules/profile/profile_page";
+import { Route, Routes } from 'react-router-dom';
+import { ModernDashboardPage } from './modern/modules/ModernDashboardPage';
+import { ModernHomePage } from './modern/modules/home/modern_home_page';
+import { ProfilePage } from './modern/modules/profile/profile_page';
+import HomePage from './classic/modules/home/HomePage';
+import ModernSignInPage from './modern/modules/auth/ModernSignInPage';
+import ModernSignUpPage from './modern/modules/auth/ModernSignUpPage';
 
 function App(): JSX.Element {
   return (
     <Routes>
-
-<Route path='/' element={<ModernDashboardPage><ProfilePage /></ModernDashboardPage>} />
+      <Route
+        path='/'
+        element={
+          <ModernDashboardPage>
+            <ModernHomePage />
+          </ModernDashboardPage>
+        }
+      />
       {/* public route */}
 
-    <Route path='/' element={<ModernHomePage />} />
-    <Route path='/profile' element={<ProfilePage />} />
-
+      <Route path='/' element={<ModernHomePage />} />
+      <Route path='/profile' element={<ProfilePage />} />
+      <Route path='/signin' element={<ModernSignInPage />} />
+      <Route path='/signup' element={<ModernSignUpPage />} />
 
       {/* protected route */}
       {/* <Route element={<AuthRequire />}>
---
+++ src/templates/modern/modules/auth/ModernSignUpPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import icon from './../../assets/images/Slogan.png';
+import classes from './ModernAuthPage.module.css';
+import InputField from './components/InputField';
+import { Link, useNavigate } from 'react-router-dom';
+import SubmitButton from './components/SubmitButton';
+
+const ModernSignUpPage = () => {
+  const navigate = useNavigate();
+
+  function submitHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    navigate('/');
+  }
+
+  return (
+    <div className={classes.background}>
+      <div className={classes.container}>
+        <img src={icon} alt='icon' />
+        <h1>Đăng ký</h1>
+        <form onSubmit={submitHandler}>
+          <InputField type='email' placeholder='Nhập email' />
+          <InputField type='password' placeholder='Nhập mật khẩu' />
+          <InputField type='password' placeholder='Nhập lại mật khẩu' />
+          <SubmitButton type='submit'>Đăng ký</SubmitButton>
+        </form>
+        <span>
+          Đã có tài khoản? <Link to='/signin'>Đăng nhập ngay</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ModernSignUpPage;
