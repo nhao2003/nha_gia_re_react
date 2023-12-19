4d201905e29a14db566847fce20b5d9768b6985e
ThachSangW3G <thachsang2202@gmail.com>
2023-12-20 03:10:40 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/*' element={<App />} />
+          <Route path='/*' element={<ModerApp />} />
         </Routes>
       </BrowserRouter>
     </Provider>
