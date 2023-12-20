c68b209452506a8df073ce42d5591873a6e09ba1
Hi <huydao1008@gmail.com>
2023-12-20 07:58:35 +0700

    update sign up

--- src/templates/modern/modules/detail/ModernDetailPage.tsx
+++ src/templates/modern/modules/detail/ModernDetailPage.tsx
@@ -126,8 +126,6 @@ export function ModernDetailPage(): JSX.Element {
     mohinh: 'Cá nhân',
   };
 
-  const navigate = useNavigate();
-
   const navigateToProfile = () => {
     if (post !== null) navigate(`/user/${post.user.id}`, { state: post.user });
   };
