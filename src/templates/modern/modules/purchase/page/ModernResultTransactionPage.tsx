a47ec19bebfeb72bae34c051e7650d88c7154cf2
minhphan46 <21522345@gm.uit.edu.vn>
2024-01-15 03:18:43 +0700

    ModernResultTransactionPage

--- src/templates/modern/modules/purchase/page/ModernResultTransactionPage.tsx
+++ src/templates/modern/modules/purchase/page/ModernResultTransactionPage.tsx
@@ -24,7 +24,7 @@ const ModernResultTransactionPage: React.FC<{ isSuccess: boolean }> = ({ isSucce
         <hr style={{ width: '100%', border: '2px solid #026D4D' }} />
       </div>
 
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '10px' }}>
         <img
           src={isSuccess ? SuccessIcon : FailIcon}
           alt='Transaction Icon'
@@ -41,32 +41,49 @@ const ModernResultTransactionPage: React.FC<{ isSuccess: boolean }> = ({ isSucce
       <div
         style={{
           display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          background: 'white',
-          border: '2px solid #026D4D',
+          background: '#F6F6F6',
           borderRadius: '8px',
           padding: '20px',
-          marginBottom: '20px',
           textAlign: 'left',
+          alignItems: 'center', // Căn giữa theo chiều dọc
+          justifyContent: 'center', // Căn giữa theo chiều ngang
+          maxWidth: '450px', // Điều chỉnh giá trị maxWidth tùy vào nhu cầu của bạn
+          margin: '0 auto', // Để căn giữa theo chiều ngang
+          marginBottom: '20px',
         }}
       >
-        <div>
-          <p>Mã giao dịch: ID29204912</p>
-          <p>Thời gian giao dịch: 14:26 21/10/2023</p>
-          <p>Loại giao dịch: Mua Gói chuyên nghiệp 1 tháng</p>
-          <p>Ngày bắt đầu: 14:27 21/10/2023</p>
-          <p>Ngày kết thúc: 14:27 21/11/2023</p>
+        <div style={{ flex: '1', color: '#959BA0' }}>
+          <p>Mã giao dịch:</p>
+          <p>Thời gian giao dịch:</p>
+          <p>Loại giao dịch:</p>
+          <p>Ngày bắt đầu:</p>
+          <p>Ngày kết thúc:</p>
+        </div>
+        <div style={{ flex: '1.5', color: '#2B3641' }}>
+          <p>ID29204912</p>
+          <p>14:26 21/10/2023</p>
+          <p>Mua Gói chuyên nghiệp 1 tháng</p>
+          <p>14:27 21/10/2023</p>
+          <p>14:27 21/11/2023</p>
         </div>
       </div>
 
-      <div style={{ display: 'flex', justifyContent: 'start', alignItems: 'center', marginBottom: '20px' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'flex-start',
+          textAlign: 'left',
+          margin: '0 auto', // Để căn giữa theo chiều ngang
+          maxWidth: '600px', // Điều chỉnh giá trị maxWidth tùy vào nhu cầu của bạn
+          marginBottom: '20px',
+        }}
+      >
         <img src={CircleIcon} alt='Circle Icon' style={{ width: '30px', height: '30px', marginRight: '10px' }} />
-        <p style={{ color: '#6A6A6A', margin: '0' }}>
-          Bạn có thể xem lại giao dịch trong lịch sử giao dịch. Nếu cần giúp đỡ có thể liên hệ qua
-          <span style={{ color: '#EB7910', marginLeft: '5px' }}>0987654321</span>
+        <p style={{ color: 'black', margin: '0' }}>
+          Bạn có thể xem lại giao dịch trong lịch sử giao dịch. Nếu cần giúp đỡ có thể liên hệ qua{' '}
+          <span style={{ color: '#EB7910' }}>0987654321 </span>
           hoặc
-          <span style={{ color: '#EB7910', marginLeft: '5px' }}>abc@abc.com</span>
+          <span style={{ color: '#EB7910' }}> abc@abc.com</span>
         </p>
       </div>
 
@@ -84,8 +101,8 @@ const ModernResultTransactionPage: React.FC<{ isSuccess: boolean }> = ({ isSucce
             height: '50px',
             boxShadow: 'none',
             border: '2px solid #4A5568',
-            marginLeft: '15px',
-            marginRight: '15px',
+            marginLeft: '20px',
+            marginRight: '20px',
           }}
         >
           Quay lại
