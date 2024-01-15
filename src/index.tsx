d643e693ab68f57733a411ec948475645ffb5c16
Hi <huydao1008@gmail.com>
2024-01-16 02:44:23 +0700

    Update route component in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/*' element={<App />} />
+          <Route path='/*' element={<ClassicApp />} />
         </Routes>
       </BrowserRouter>
     </Provider>
