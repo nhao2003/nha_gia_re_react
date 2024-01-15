fa3cd757090032818323b2f0f8ec04f611998661
nhao2003 <nhathaodev@gmail.com>
2024-01-15 13:36:55 +0700

    Add content to About Us page

--- src/templates/modern/modules/aboutUs/page/ModernAboutUsPage.tsx
+++ src/templates/modern/modules/aboutUs/page/ModernAboutUsPage.tsx
@@ -6,6 +6,16 @@ function ModernAboutUsPage(): JSX.Element {
     return (
         <div>
             <h1>About us page</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, voluptatum. Quisquam, quidem
+                voluptas. Quisquam, quidem voluptas.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, voluptatum. Quisquam, quidem
+                voluptas. Quisquam, quidem voluptas.
+            </p>
         </div>
     );
 }
