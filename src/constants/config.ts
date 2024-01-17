89ffa45c9d25834181e477d25324aff7e3cb71ed
nhao2003 <nhathaodev@gmail.com>
2024-01-17 13:13:44 +0700

    Update index.tsx and config.ts, and add checkLimitPost() method to PostService

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 class AppConfig {
-  public readonly isProduction = true;
+  public readonly isProduction = false;
   public readonly PRODUCTION_API_URL = 'https://nha-gia-re-server.onrender.com/api/v1';
   public readonly DEVELOPMENT_API_URL = 'http://localhost:8000/api/v1';
   public readonly API_URL = this.isProduction ? this.PRODUCTION_API_URL : this.DEVELOPMENT_API_URL;
